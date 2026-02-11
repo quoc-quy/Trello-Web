c519b98ab6cc8af388559a9e8abe4a2ad4a2ad69
quoc-quy <quyleom10@gmail.com>
2026-02-11 20:15:17 +0700

    feat: add system mode

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,51 @@ import { pink } from '@mui/material/colors'
 import { Typography } from '@mui/material'
 
 import { useColorScheme } from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery'
+
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeIcon from '@mui/icons-material/DarkMode'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+  const handleChange = (event) => {
+    setMode(event.target.value)
+  }
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <LightModeIcon /> Light
+        </MenuItem>
+        <MenuItem value="dark">
+          <DarkModeIcon /> Dark
+        </MenuItem>
+        <MenuItem value="system">
+          <SettingsBrightnessIcon /> System
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
 
 function ModeToggle() {
   const { mode, setMode } = useColorScheme()
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+  console.log(prefersDarkMode)
+
   return (
     <Button
       onClick={() => {
@@ -23,7 +65,10 @@ function ModeToggle() {
 function App() {
   return (
     <>
+      <ModeSelect />
+      <hr />
       <ModeToggle />
+      <hr />
       <div>hello</div>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
